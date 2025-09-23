5425a01c5a5b94a457a8773597b9ce745c8f5b6f
Fra <fgrieco9045@gmail.com>
2025-09-23 07:47:34 +0200

    remove from sidebar filers for tipologia

--- template-parts/search/filters.php
+++ template-parts/search/filters.php
@@ -15,44 +15,7 @@ if(isset($_GET["post_terms"]))
 ?>
 
 <div class="col-lg-3 d-none d-lg-block scroll-filter-wrapper">
-    <h2 class="visually-hidden" id="filter">filtri da applicare</h2>
-    <fieldset>
-        <legend class="h6 text-uppercase category-list__title">Tipologie</legend>
-        <div class="categoy-list pb-4">
-            <ul>
-                <?php 
-                    foreach ($tipologie as $type_slug) {
-                        $tipologia = get_term_by('slug', $type_slug);
-
-                        if ( isset( COMUNI_TIPOLOGIE[$type_slug] ) && isset( COMUNI_TIPOLOGIE[$type_slug]['plural_name'] ) ) {
-                            $plural_name = COMUNI_TIPOLOGIE[$type_slug]['plural_name'];
-                        } else if ( post_type_exists( $type_slug ) ) {
-                            $plural_name = get_post_type_object( $type_slug )->labels->name;
-                        }
-                ?>
-                <li>
-                    <div class="form-check">
-                        <div class="checkbox-body border-light py-3">
-                        <input
-                            type="checkbox"
-                            name="post_types[]"
-                            <?php if(in_array($type_slug, $post_types)) echo " checked "; ?>
-                            onChange="this.form.submit()"                            
-                            id="<?php echo $type_slug; ?>" 
-                            value="<?php echo $type_slug; ?>" 
-                        />
-                        <label
-                            for="<?php echo $type_slug; ?>" 
-                            class="subtitle-small_semi-bold mb-0 category-list__list"
-                            ><?php echo $plural_name; ?> 
-                        </label>
-                        </div>
-                    </div>
-                </li>
-                <?php } ?>
-            </ul>
-        </div>
-    </fieldset>    
+    <h2 class="visually-hidden" id="filter">filtri da applicare</h2>   
     <fieldset>
         <legend class="h6 text-uppercase category-list__title">Argomenti</legend>
         <div class="categoy-list pb-4">
