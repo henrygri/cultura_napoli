c269b6ac643fb87aa547e2a01109f75eded133ba
SalvatoreRizzello <rizzello.salvatore@gmail.com>
2025-10-15 15:53:38 +0200

    Mappa quartieri: collego dati dinamici e seleziono random una municipalità next: fare bene le card dei luoghi e cambiare colonna sinistra al click della mappa

--- home.php
+++ home.php
@@ -44,6 +44,45 @@ get_header();
 
         <?php get_template_part("template-parts/common/bolli-argomenti"); ?>
 
+
+        <?php
+        // 1. Recupera tutti i termini di primo livello
+        $municipalita = get_terms([
+          'taxonomy' => 'quartieri',
+          'parent'   => 0,
+          'hide_empty' => true,
+        ]);
+
+        if (!empty($municipalita) && !is_wp_error($municipalita)) {
+
+          // 2. Scegli una municipalità random
+          $term_random = $municipalita[array_rand($municipalita)];
+          $slug_municipalita = $term_random->slug;
+
+          // 3. Recupera i quartieri figli
+          $quartieri_figli = get_terms([
+            'taxonomy' => 'quartieri',
+            'parent'   => $term_random->term_id,
+            'hide_empty' => false,
+          ]);
+
+          // 4. Recupera 6 post random di tipo 'luogo' associati
+          $luoghi = get_posts([
+            'post_type' => 'luogo',
+            'posts_per_page' => 6,
+            'orderby' => 'rand',
+            'tax_query' => [
+              [
+                'taxonomy' => 'quartieri',
+                'field'    => 'term_id',
+                'terms'    => [$term_random->term_id],
+                'include_children' => true,
+              ],
+            ],
+          ]);
+        }
+        ?>
+
         <section class="py-5 bg-200">
           <div class="container">
             <div class="row py-3">
@@ -51,85 +90,31 @@ get_header();
                 <h2>I Luoghi della cultura</h2>
               </div>
               <div class="col-lg-6 pe-5 pt-3 pt-lg-5">
-                <?php get_template_part("template-parts/luogo/mappa-svg"); ?>
+                <?php get_template_part('template-parts/luogo/mappa-svg', null, [ 'slug_municipalita' => $slug_municipalita ]); ?>
               </div>
               <div class="col-lg-6 ps-5 pt-3 pt-lg-5">
-                <h3>Municipalità 2</h3>
-                <p class="text-secondary">Avvocata, Montecalvario, Pendino, Porto, Mercato, San Giuseppe</p>
+                <h3><?php echo esc_html($term_random->name); ?></h3>
+                <?php if (!empty($quartieri_figli)) : ?>
+                  <p class="text-secondary">
+                    <?php echo implode(', ', wp_list_pluck($quartieri_figli, 'name')); ?>
+                  </p>
+                <?php endif; ?>
                 <div class="row">
-
+                  <?php foreach ($luoghi as $luogo) : ?>
                   <div class="col-md-6">
                     <div class="card rounded no-after mb-3 card-luogo-small bg-white">
                       <div class"card-img-wrapper">
-                        <div class="card-img"></div>
+                        <div class="card-img">
+                          <?php echo get_the_post_thumbnail($luogo->ID, 'medium'); ?>
+                        </div>
                       </div>
                       <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
+                        <h4 class="h5"><?php echo esc_html(get_the_title($luogo)); ?></h4>
+                        <p class="mb-0"><?php echo esc_html(get_post_meta($luogo->ID, 'indirizzo', true)); ?></p>
                       </div>
                     </div>
                   </div>
-
-                  <div class="col-md-6">
-                    <div class="card rounded no-after mb-3 card-luogo-small bg-white">
-                      <div class"card-img-wrapper">
-                        <div class="card-img"></div>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                    <div class="card rounded no-after mb-3 card-luogo-small bg-white">
-                      <div class"card-img-wrapper">
-                        <div class="card-img"></div>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                    <div class="card rounded no-after mb-3 card-luogo-small bg-white">
-                      <div class"card-img-wrapper">
-                        <div class="card-img"></div>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                    <div class="card rounded no-after mb-3 card-luogo-small bg-white">
-                      <div class"card-img-wrapper">
-                        <div class="card-img"></div>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                    <div class="card rounded no-after mb-3 card-luogo-small bg-white">
-                      <div class"card-img-wrapper">
-                        <div class="card-img"></div>
-                      </div>
-                      <div class="card-body">
-                        <h4 class="h5">Nome Luogo</h4>
-                        <p class="mb-0">Nome della strada, 00, 80100</p>
-                      </div>
-                    </div>
-                  </div>
-
+                  <?php endforeach; ?>
 
                 </div>
               </div>
@@ -137,21 +122,21 @@ get_header();
           </div>
         </section>
 
+        <?php /*
         <section id="head-section">
             <h2 class="visually-hidden">Contenuti in evidenza</h2>
             <?php
-			$messages = dci_get_option( "messages", "home_messages" );
-            if($messages && !empty($messages)) {
-                get_template_part("template-parts/home/messages");
-            }
-		    ?>
+          			$messages = dci_get_option( "messages", "home_messages" );
+                if($messages && !empty($messages)) {
+                    get_template_part("template-parts/home/messages");
+                }
+    		    ?>
             <?php get_template_part("template-parts/home/notizie"); ?>
             <?php get_template_part("template-parts/home/calendario"); ?>
         </section>
+        */ ?>
 
-        <?php get_template_part("template-parts/common/bolli-argomenti"); ?>
-
-
+        <?php /*
         <section id="evidenza" class="evidence-section">
             <div class="section py-5 pb-lg-80 px-lg-5 position-relative" style="<?php if (file_exists(get_stylesheet_directory().'/assets/img/evidenza-header.png')){ ?>background-image: url('<?php echo esc_url( get_stylesheet_directory_uri()); ?>/assets/img/evidenza-header.png');<?php }else{ ?>background-image: url('<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } ?>">
                 <?php get_template_part("template-parts/home/argomenti"); ?>
@@ -159,8 +144,9 @@ get_header();
             </div>
         </section>
         <?php get_template_part("template-parts/home/ricerca"); ?>
+        */?>
 
-        <?php get_template_part("template-parts/evento/evidenza"); ?>
+        <?php get_template_part("template-parts/evento/prossimi-eventi"); ?>
 
     </main>
 <?php
