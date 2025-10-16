c431286f41b0fc6942aff05f9a4f8902789cc4f4
Fra <fgrieco9045@gmail.com>
2025-10-16 23:27:56 +0200

    style pagination slider

--- template-parts/home/slideshow.php
+++ template-parts/home/slideshow.php
@@ -3,18 +3,21 @@
   $schede = dci_get_option('schede_evidenziate', 'homepage') ?? null;
 
   if ($schede && count($schede) > 0) { ?>
-  <div class="splide slider_home">
-      <div class="splide__track">
-        <div class="splide__list">
-          <?php
-          foreach ($schede as $scheda) {
-              if ($scheda) {
-                  echo '<div class="splide__slide p-3">';
-                  get_template_part("template-parts/home/scheda-evidenza-full");
-                  echo '</div>';
-              }
-          } ?>
-        </div>
-      </div>
+  <div class="col-12">
+    <div class="splide slider_home">
+          <div class="splide__track">
+            <div class="splide__list">
+              <?php
+              foreach ($schede as $scheda) {
+                  if ($scheda) {
+                      echo '<div class="splide__slide">';
+                      get_template_part("template-parts/home/scheda-evidenza-full");
+                      echo '</div>';
+                  }
+              } ?>
+            </div>
+          </div>
+    </div>
   </div>
+ 
 <?php } ?>
