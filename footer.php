95285f19e47b878cdaa62b12a1a20750f65daee3
SalvatoreRizzello <rizzello.salvatore@gmail.com>
2026-03-17 22:26:24 +0100

    nuovo footer

--- footer.php
+++ footer.php
@@ -15,27 +15,22 @@
     <div class="it-footer-main">
         <div class="container">
             <div class="row">
-                <div class="col-12 footer-items-wrapper logo-wrapper">
-                <img class="ue-logo" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea">
+              <div class="col-12 footer-items-wrapper logo-wrapper">
                 <div class="it-brand-wrapper">
-                  <a href="https://www.comune.napoli.it" class="" data-focus-mouse="false">
-                    <svg width="82" height="82" class="icon" aria-hidden="true">
-                      <image xlink:href="https://static-www.comune.napoli.it/wp-content/uploads/2024/03/Logo-Napoli.png" width="82" height="82"></image>
-                    </svg>
-                    <div class="it-brand-text">
-                      <h2 class="no_toc">Comune di Napoli</h2>
-                    </div>
+                  <a target="_blank" href="https://www.comune.napoli.it/">
+                    <img class="logo-comune icon" src="https://static-www.comune.napoli.it/wp-content/uploads/2024/03/Logo-Napoli.png" alt="logo Comune di Napoli">
                   </a>
                 </div>
                 <div class="it-brand-wrapper">
                   <a href="<?php echo home_url() ?>">
-                      <?php get_template_part("template-parts/common/logo");?>
-                      <?php /*
-                      <div class="it-brand-text">
-                          <h2 class="no_toc"><?php echo dci_get_option("nome_comune");?></h2>
-                      </div>
-                      */ ?>
+                    <img class="logo-comune icon" src="<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-cultura-napoli-color.svg" alt="logo Cultura Napoli">
                   </a>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 text-uppercase">
+                PORTALE TEMATICO DELL’AREA CULTURA DEL COMUNE DI NAPOLI
               </div>
             </div>
             <div class="row">
@@ -126,14 +121,14 @@
                     <h3 class="footer-heading-title">Contatti</h3>
                     <div class="row">
                       <div class="col-md-6">
-                        <strong>Area Cultura</strong><br>
-                        Sede di Palazzo Cavalcanti<br>
-                        Via Toledo 348 - 80134 - Napoli
+                        <strong>Comune di Napoli - Area Cultura</strong><br>
+                        Palazzo Cavalcanti<br>
+                        Via Toledo 348 - 80134 Napoli
                       </div>
                       <div class="col-md-6">
-                        <br>
-                        <strong>Telefono:</strong> 0039 0817955237<br>
-                        <strong>E-mail:</strong> area.cultura.turismo@comune.napoli.it
+                        <strong>Telefono:</strong> 0039 081 7955237<br>
+                        <strong>E-mail:</strong> <a target="_blank" href="mailto:area.cultura.turismo@comune.napoli.it">area.cultura.turismo@comune.napoli.it</a><br>
+                        Vai alla pagina dedicata su <a target="_blank" href="https://comune.napoli.it">comune.napoli.it</a>
                       </div>
                       <?php /*
                         <div class="col-md-4">
@@ -205,11 +200,17 @@
                                 <?php } ?>
                             </ul><!-- /header-social-wrapper -->
                     <?php } ?>
+                    <p>
+                        <a href="https://cultura.comune.napoli.it/newsletter/">Iscriviti alla newsletter</a><br>
+                        <a href="https://cultura.comune.napoli.it/press-kit/">Consulta l'Area stampa</a><br>
+                        <a href="https://cultura.comune.napoli.it/tipi_notizia/bandi-e-avvisi-pubblici/">Consulta l'Area bandi</a>
+                    </p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 footer-items-wrapper">
                     <div class="footer-bottom">
+                        © 2026 Comune di Napoli | Sito realizzato utilizzando il <a target="_blank" href="https://designers.italia.it/modelli/comuni/">Modello Comuni di Designers Italia</a>
 						<?php if(dci_get_option("media_policy",'footer')) { ?>
 							<a href="<?php echo dci_get_option("media_policy",'footer'); ?>">Media policy</a>
 						<?php } ?>
